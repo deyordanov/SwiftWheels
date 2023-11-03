0c445473ba039adc4ee6d68aee5eaeecbb929bfa
Denis Yordanov <deyordanov777@gmail.com>
2023-11-03 09:17:31 +0200

    Importing the components

--- swift-wheels/src/app/page.tsx
+++ swift-wheels/src/app/page.tsx
@@ -1,4 +1,21 @@
-import Image from "next/image";
+//Components
+import About from "./Components/About/About";
+import BackToTopBtn from "./Components/BackToTopBtn/BackToTopBtn";
+import Brands from "./Components/Brands/Brands";
+import Cars from "./Components/Cars/Cars";
+import CarSlider from "./Components/CarSlider/CarSlider";
+import Copyright from "./Components/Copyright/Copyright";
+import Cta from "./Components/Cta/Cta";
+import DateSelection from "./Components/DateSelection/DateSelection";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+import Hero from "./Components/Hero/Hero";
+import HoursSelection from "./Components/HoursSelection/HoursSelection";
+import LocationSelection from "./Components/LocationSelection/LocationSelection";
+import Search from "./Components/Search/Search";
+import Testimonial from "./Components/Testimonial/Testimonial";
+import TestimonialSlider from "./Components/TestimonialSlider/TestimonialSlider";
+import Why from "./Components/Why/Why";
 
 export default function Home() {
   return <main className="">main</main>;
