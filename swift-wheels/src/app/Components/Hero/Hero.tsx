e196ae69d208f0960bbddce4c77a29dc5b1a3aad
Denis Yordanov <deyordanov777@gmail.com>
2023-11-03 21:51:46 +0200

    Update Her and Search components

--- swift-wheels/src/app/Components/Hero/Hero.tsx
+++ swift-wheels/src/app/Components/Hero/Hero.tsx
@@ -1,3 +1,25 @@
+"use client";
+
+//Components
+import Search from "../Search/Search";
+
+//Contexts
+import { useSearchContext } from "@/app/Contexts/searchContext";
+
 export default function Hero() {
-  return <div>Hero</div>;
+  const { searchActive } = useSearchContext();
+  return (
+    <section className="h-screen xl:h-[90vh] bg-[#b2b7c2]/10" id="home">
+      <div className="container mx-auto h-full xl:pt-10">hero container</div>
+      <div
+        className={`${
+          searchActive
+            ? "fixed top-[140px] z-10 w-full max-w-[1920px]"
+            : "-mt-12 w-full max-w-[1300px] mx-auto"
+        }`}
+      >
+        <Search />
+      </div>
+    </section>
+  );
 }
--- swift-wheels/src/app/Components/Search/Search.tsx
+++ swift-wheels/src/app/Components/Search/Search.tsx
@@ -1,3 +1,24 @@
+"use client";
+
+//Components
+import LocationSelection from "../LocationSelection/LocationSelection";
+
+//Contexts
+import { useSearchContext } from "../../Contexts/searchContext";
+
 export default function Search() {
-  return <div>Search</div>;
+  const { searchActive } = useSearchContext();
+  return (
+    <div
+      className={`${
+        searchActive
+          ? "rounded-none xl:h-[80px]"
+          : "rounded-[20px] py-6 xl:pr-4 xl:h-[98px]"
+      } bg-white hidden xl:block w-full relative shadow-lg`}
+    >
+      <div className="xl:h-full flex items-center px-6 xl:px-0 ">
+        <LocationSelection />
+      </div>
+    </div>
+  );
 }
