d1391b5bd1f2d5cbf37ef0c669cdca8a0038ecc0
Denis Yordanov <deyordanov777@gmail.com>
2023-11-04 09:36:09 +0200

    Added the location selection window - you can pick a location now

--- swift-wheels/src/app/Components/LocationSelection/LocationSelection.tsx
+++ swift-wheels/src/app/Components/LocationSelection/LocationSelection.tsx
@@ -1,3 +1,61 @@
+"use client";
+
+//hooks
+import { useState } from "react";
+
+//headless ui
+import { Menu } from "@headlessui/react";
+
+//react-icons
+import { FaMapMarkerAlt } from "react-icons/fa";
+
+//locations
+const locations = [
+  "1000 Kingsway, Canada",
+  "2200 Autobahn Ave, Germany",
+  "58 Rue de l'Automobile, France",
+  "15 Car Dealership Road, Australia",
+  "88 Vehicular Street, Japan",
+];
+
 export default function LocationSelection() {
-  return <div>LocationSelection</div>;
+  const [location, setLocation] = useState("Select Location");
+
+  const handleLocation = (location: string) => {
+    setLocation(location);
+  };
+
+  return (
+    <Menu as="div" className="w-full h-full flex xl:flex-row">
+      <div className="relative flex-1">
+        <Menu.Button className="dropdown-btn w-full h-full flex flex-col justify-center items-center xl:items-start xl:pl-8">
+          <div className="w-full h-16 xl:h-full flex justify-center xl:justify-start xl:border-r xl:border-black/10">
+            <div className="flex flex-col justify-center">
+              <div className="flex flex-col xl:flex-row items-center xl:gap-x-2 gap-y-2 xl:gap-y-0">
+                <FaMapMarkerAlt className="text-accent-default" />
+                <span className="text-[15px] uppercase font-bold">
+                  Select Location
+                </span>
+              </div>
+              <span className="uppercase font-medium text-[13px] text-secondary text-center xl:ml-6 xl:text-left">
+                {location}
+              </span>
+            </div>
+          </div>
+        </Menu.Button>
+        <Menu.Items className=" dropdown-menu shadow-lg absolute -top-80 xl:top-[90px] left-1/2 xl:left-0 z-10 transform -translate-x-1/2 xl:-translate-x-0 text-sm text-center xl:text-left w-full bg-white max-w-[332px] py-6 rounded-[10px]">
+          {locations.map((location, index) => (
+            <li
+              onClick={() => handleLocation(location)}
+              key={index}
+              className="list-none cursor-pointer py-4 xl:pl-10 hover:bg-gray-50 text-[13px] uppercase"
+              aria-hidden
+            >
+              {location}
+            </li>
+          ))}
+        </Menu.Items>
+      </div>
+    </Menu>
+  );
 }
