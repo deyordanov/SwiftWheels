06ebbc616fadd98cd2353e712a7ff6e1ec914e22
Denis Yordanov <deyordanov777@gmail.com>
2023-11-04 09:37:15 +0200

    Updated SearchMobile component and added default values for setDate func in constants

--- swift-wheels/src/app/utilities/constants/constans.ts
+++ swift-wheels/src/app/utilities/constants/constans.ts
@@ -1,7 +1,16 @@
 //types
 import * as searchContextTypes from "../types/searchContext.types";
+import { Range } from "react-date-range";
 
+//Search Context
 export const searchContextDefaultValues: searchContextTypes.defaultTypes = {
   searchActive: false,
   setSearchActive: () => {}, //empty func
+};
+
+//Date Selection
+export const setDateDefaultValues: Range = {
+  startDate: new Date(),
+  endDate: undefined,
+  key: "selection",
 };
