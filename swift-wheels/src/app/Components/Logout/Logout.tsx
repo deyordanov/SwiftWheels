b47c167380005e29d97bb98291ce3dd493c77e19
Denis Yordanov <deyordanov777@gmail.com>
2023-11-08 04:34:50 +0200

    Added the Logout component

---
+++ swift-wheels/src/app/Components/Logout/Logout.tsx
@@ -0,0 +1,71 @@
+//headless ui
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
+
+//types
+import * as LogoutTypes from "@/app/utilities/types/logout.types";
+
+export default function Logout({ handleLogout }: LogoutTypes.propTypes) {
+  let [isOpen, setIsOpen] = useState(true);
+
+  function closeModal() {
+    setIsOpen(false);
+    handleLogout();
+  }
+
+  return (
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={closeModal}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black/25" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Title
+                  as="h3"
+                  className="text-lg font-medium leading-6 text-gray-900"
+                >
+                  <b>Logout</b>
+                </Dialog.Title>
+                <div className="mt-2">
+                  <p className="text-sm text-secondary">
+                    Are you sure you want to logout of your account?
+                  </p>
+                </div>
+
+                <div className="mt-4">
+                  <button
+                    type="button"
+                    className="inline-flex justify-center rounded-md border border-transparent bg-accent-default px-4 py-2 text-sm font-medium text-white hover:bg-accent-hover focus:outline-none"
+                    onClick={closeModal}
+                  >
+                    Logout
+                  </button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+}
