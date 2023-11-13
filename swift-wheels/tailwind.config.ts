aa19ae9753e47c29aa739f7964016d93ea64dfe2
Denis Yordanov <deyordanov777@gmail.com>
2023-11-13 15:04:36 +0200

    Implementing Uploadthing for image storage

--- swift-wheels/tailwind.config.ts
+++ swift-wheels/tailwind.config.ts
@@ -1,42 +1,48 @@
 import type { Config } from "tailwindcss";
 
+import { withUt } from "uploadthing/tw";
+
 const withMT = require("@material-tailwind/react/utils/withMT");
 
-const config: Config = withMT({
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    container: {
-      padding: {
-        DEFAULT: "15px",
-      },
-    },
-    screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1300px",
-    },
-    extend: {
-      colors: {
-        primary: "#101828",
-        secondary: "#667085",
-        accent: {
-          default: "#ed1d24",
-          hover: "#dd242a",
+const config: Config = withUt(
+    withMT({
+        content: [
+            "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+            "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+            "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+            "./src/**/*.{ts,tsx,mdx}",
+        ],
+        theme: {
+            container: {
+                padding: {
+                    DEFAULT: "15px",
+                },
+            },
+            screens: {
+                sm: "640px",
+                md: "768px",
+                lg: "1024px",
+                xl: "1300px",
+            },
+            extend: {
+                colors: {
+                    primary: "#101828",
+                    secondary: "#667085",
+                    accent: {
+                        default: "#ed1d24",
+                        hover: "#dd242a",
+                    },
+                    body: "dedede",
+                },
+                backgroundImage: {
+                    "gradient-radial":
+                        "radial-gradient(var(--tw-gradient-stops))",
+                    "gradient-conic":
+                        "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+                },
+            },
         },
-        body: "dedede",
-      },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
-});
+        plugins: [],
+    })
+);
 export default config;
