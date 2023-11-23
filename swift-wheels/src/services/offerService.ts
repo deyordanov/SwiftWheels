1c1d34da4c6521a840ac80398a8e1fb017bc6fcc
Denis Yordanov <deyordanov777@gmail.com>
2023-11-23 23:55:10 +0200

    Created and added the functionality for the YourOffers component

---
+++ swift-wheels/src/services/offerService.ts
@@ -0,0 +1,30 @@
+//requester
+import * as requester from "./requester";
+
+//shared
+import { getAuthHeaders } from "@/app/utilities/shared/shared";
+
+const baseUrl = "http://localhost:3030/data/offers";
+
+export const create = async (data: object) => {
+    const headers = getAuthHeaders(false);
+    const response = await requester.authorizationPost(
+        headers,
+        JSON.stringify({ ...data }),
+        baseUrl
+    );
+    return response;
+};
+
+export const getAllFilter = async (sellerId: string) => {
+    console.log(sellerId);
+    const headers = getAuthHeaders(false);
+    const query = encodeURIComponent(`sellerId="${sellerId}"`);
+    const offers = await requester.authorizationGet(
+        headers,
+        {},
+        `${baseUrl}?where=${query}`
+    );
+
+    return offers;
+};
