b3a92655c11b84f5b84e25b693204219ea97475b
Denis Yordanov <deyordanov777@gmail.com>
2023-12-01 22:37:05 +0200

    Fixed a bug with the dependencies

--- swift-wheels/src/app/YourOffers/OfferFilter/OfferFilter.tsx
+++ swift-wheels/src/app/YourOffers/OfferFilter/OfferFilter.tsx
@@ -1,66 +1,45 @@
+import { Menu, Transition } from "@headlessui/react";
+import Link from "next/link";
 import * as React from "react";
-import Button from "@mui/material/Button";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import Fade from "@mui/material/Fade";
-
-import { FaFilter } from "react-icons/fa";
-import Checkbox from "@mui/material/Checkbox";
-import FormControlLabel from "@mui/material/FormControlLabel";
+import { Fragment } from "react";
+import { BiSolidUser } from "react-icons/bi";
 
 export default function FadeMenu() {
-    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-        setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
-
     return (
-        <div className="text-primary">
-            <Button
-                id="fade-button"
-                aria-controls={open ? "fade-menu" : undefined}
-                aria-haspopup="true"
-                aria-expanded={open ? "true" : undefined}
-                onClick={handleClick}
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            >
-                <FaFilter className="ml-1 text-2xl" />
-            </Button>
-
-            <Menu
-                id="fade-menu"
-                MenuListProps={{
-                    "aria-labelledby": "fade-button",
-                }}
-                anchorEl={anchorEl}
-                open={open}
-                onClose={handleClose}
-                TransitionComponent={Fade}
-            >
-                <div className="flex flex-col">
-                    <FormControlLabel
-                        value="start"
-                        control={<Checkbox defaultChecked color="success" />}
-                        label="Pending"
-                        labelPlacement="end"
-                    />
-                    <FormControlLabel
-                        value="start"
-                        control={<Checkbox defaultChecked color="success" />}
-                        label="Accepted"
-                        labelPlacement="end"
-                    />
-                    <FormControlLabel
-                        value="start"
-                        control={<Checkbox defaultChecked color="success" />}
-                        label="Declined"
-                        labelPlacement="end"
-                    />
+        <div className="text-right cursor-pointer">
+            <Menu as="div" className="text-left">
+                <div>
+                    <Menu.Button className="inline-flex justify-center px-4 py-2 text-sm font-medium ">
+                        <BiSolidUser className="text-2xl" />
+                    </Menu.Button>
                 </div>
+                <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                >
+                    <Menu.Items className=" absolute mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+                        <div className="px-1 py-1">
+                            <Menu.Item>
+                                <div>Hello</div>
+                            </Menu.Item>
+                        </div>
+                        <div className="px-1 py-1 ">
+                            <Menu.Item>
+                                <div>Hello</div>
+                            </Menu.Item>
+                        </div>
+                        <div className="px-1 py-1">
+                            <Menu.Item>
+                                <div>Hello</div>
+                            </Menu.Item>
+                        </div>
+                    </Menu.Items>
+                </Transition>
             </Menu>
         </div>
     );
